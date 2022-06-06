ab373c2eaf07eaf6fa2460cf5fc97d62a92ce95b
SaiKeerthanaG <saikeerthana.1919@gmail.com>
2022-06-06 02:45:11 -0700

    Initial Commit

---
+++ ABCApp/Model/DBUtilities.swift
@@ -0,0 +1,30 @@
+//
+//  DBUtilities.swift
+//  ABCApp
+//
+//  Created by Capgemini_DA204 on 6/1/22.
+//
+
+import Foundation
+
+class DBUtilities {
+    
+    private init() {}
+    static let instance = DBUtilities()
+func addStudent(name: String, rollno: Int, dob: Date) {
+    //adding student - insert
+}
+
+func updateStudent(std: Student) {
+    //update
+}
+
+func deleteStudent(std: Student) {
+    //delete
+}
+
+func getAllStudents() -> [Student] {
+    //fetch
+     return []
+}
+}
